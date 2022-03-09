fa359487b1fc055bcde03a2d175f9de7efc3a368
Hillary Muller <hillaryaward@gmail.com>
2022-03-09 08:11:15 -0500

    commit tree db35b4b2b1c02930c6670314d66ffe0de8c6ccc5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    name: "Hillary",
+    streetAddress: "2015 Bellefontaine Street"
+};
+function updateEmployeeWithKeyAndValue(employee, streetAddress, Broadway){
+    return{
+        ...employee,
+        [streetAddress]: "11 Broadway",
+    };
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, twelveBroadway){
+    employee[streetAddress] = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = { ...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name;
+    return employee;
+}
